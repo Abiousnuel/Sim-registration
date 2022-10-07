d073f9f4eb1e61c0b038836c363470b1cd27e249
Abiona Oluwadunsin Emmanuel <abionaemmanuel47@gmail.com>
2022-10-07 11:02:18 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Enter ussd");
+// string ussd = (Console.ReadLine());
+// if(ussd ==  "*777#")
+// {
+//     Console.WriteLine("Ussd running");
+// }
+// else
+// {
+//     Console.WriteLine("Wrong input. Try Again");
+// }
+// Console.WriteLine("Data bundle");
+// Console.WriteLine("1500/8GB/7days");
+// Console.WriteLine("500/3GB/4days");
+// Console.WriteLine("More Data");
+// Console.WriteLine("Family plan");
+// Console.WriteLine("Enter option");
+
+// string one = (Console.ReadLine());
+// Console.WriteLine("2GB/300/Daily");
+// Console.WriteLine("4GB/500/Daily");
+// Console.WriteLine("8GB/1500/7Days");
+// Console.WriteLine("1GB/500/14days");
+// Console.WriteLine("3GB/1000/Mntly");
+// Console.WriteLine("4GB/1200/Mntly");
+// Console.WriteLine("5GB/1500/Mntly");
+// Console.WriteLine("22 . Back");
+// Console.WriteLine("0 . Menu");
+// if(one == "22 . Back")
+// {
+// Console.WriteLine("Data bundle");
+// Console.WriteLine("1500/8GB/7days");
+// Console.WriteLine("500/3GB/4days");
+// Console.WriteLine("More Data");
+// Console.WriteLine("Family plan");
+// Console.WriteLine("Enter option");
+// }
+// if(one == "0 . Menu")
+// {
+    
+// Console.WriteLine("Data bundle");
+// Console.WriteLine("1500/8GB/7days");
+// Console.WriteLine("500/3GB/4days");
+// Console.WriteLine("More Data");
+// Console.WriteLine("Family plan");
+// Console.WriteLine("Enter option");
+// }
+// if( one == " 2GB/300/Daily" )
+// {
+//     Console.WriteLine("2GB/300/Daily . What should happen when your bundle finishes.");
+//     Console.WriteLine(" 1) CONTINUE BROWSING FROM AIRTIME/300MB EXTRA");
+//     Console.WriteLine(" 2) Stop My Data");
+//     Console.WriteLine("22 . Back");
+//     Console.WriteLine( "0 . Menu");
+// }
+// if(" amount" != one )
+// {
+//     Console.WriteLine("Insufficient Balance");
+// }
+// if(" amount" != one)
+// {
+//  Console.WriteLine("Recharge Successful");
+// }
+// if( one == "22 . Back")
+// {
+//     Console.WriteLine("2GB/300/Daily");
+// Console.WriteLine("4GB/500/Daily");
+// Console.WriteLine("8GB/1500/7Days");
+// Console.WriteLine("1GB/500/14days");
+// Console.WriteLine("3GB/1000/Mntly");
+// Console.WriteLine("4GB/1200/Mntly");
+// Console.WriteLine("5GB/1500/Mntly");
+// Console.WriteLine("22 . Back");
+// Console.WriteLine("0 . Menu");
+// }
+// if( one == "0 . Menu" )
+// {
+// Console.WriteLine("Data bundle");
+// Console.WriteLine("1500/8GB/7days");
+// Console.WriteLine("500/3GB/4days");
+// Console.WriteLine("More Data");
+// Console.WriteLine("Family plan");
+// Console.WriteLine("Enter option");
+// }
+
+
+
